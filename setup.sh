84a406ef9cbadbafe5f57e7d90bd5740d5205964
rek292 <83674893+rek292@users.noreply.github.com>
2021-05-05 16:19:34 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # setup ngrok
 
-sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip && rm ngrok-stable-linux-amd64.zip
+sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && sudo unzip ngrok-stable-linux-amd64.zip && sudo rm -rf ngrok-stable-linux-amd64.zip
 sudo chmod +x ngrok
-./ngrok authtoken 1Wxj5KuPExFLwdtvYF0KPUgPVgb_6qXeckNfuKY2CL8Z5uxyr
-rm setup.sh
+sudo ./ngrok authtoken 1Wxj5KuPExFLwdtvYF0KPUgPVgb_6qXeckNfuKY2CL8Z5uxyr
+sudo rm -rf setup.sh
 exit
