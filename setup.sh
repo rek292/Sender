caf730e16a89a1bf0ccf6858d3e74c6a4345ca3a
rek292 <83674893+rek292@users.noreply.github.com>
2021-05-05 16:04:56 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# setup ngrok
+
+sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip && rm ngrok-stable-linux-amd64.zip
+sudo chmod +x ngrok
+./ngrok authtoken 1Wxj5KuPExFLwdtvYF0KPUgPVgb_6qXeckNfuKY2CL8Z5uxyr
+exit
